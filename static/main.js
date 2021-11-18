3dee7a3897e53484859095ed6e00fbd5db9cd02e
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-18 20:54:51 +0300

    Putting text behind images in portfolio

--- static/main.js
+++ static/main.js
@@ -17,8 +17,11 @@ $("#work1_items").hover(mouseEnter, mouseLeave);
 
 function mouseEnter() {
     $("#work1_text").fadeIn(1000)
+    $("#work1_img").stop().animate({ "opacity": " 0.5" });
 };
 
 function mouseLeave() {
     $("#work1_text").fadeOut(1000)
+    $("#work1_img").stop().animate({ "opacity": " 1" });
+
 };
