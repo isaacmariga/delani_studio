a2c9caf023a05dac06f560b8665fd6a2b8f2fad6
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-18 19:42:09 +0300

    breaking portfolio into divs

--- static/main.js
+++ static/main.js
@@ -1,19 +1,24 @@
 $("#design_items").click(function() {
-    $("#design_text").toggle();
-    $("#design_img").toggle();
+    $("#design_text").toggle()
+    $("#design_img").toggle()
 });
 
 $("#development_items").click(function() {
-    $("#development_text").toggle();
-    $("#development_img").toggle();
+    $("#development_text").toggle()
+    $("#development_img").toggle()
 });
 
 $("#product_items").click(function() {
-    $("#product_text").toggle();
-    $("#product_img").toggle();
+    $("#product_text").toggle()
+    $("#product_img").toggle()
 });
 
-$("#product_items").click(function() {
-    $("#product_text").toggle();
-    $("#product_img").toggle();
-});
+$("#work1_items").hover(mouseEnter, mouseLeave);
+
+function mouseEnter() {
+    $("#work1_text").fadeIn(1000)
+};
+
+function mouseLeave() {
+    $("#work1_text").fadeOut(1000)
+};
