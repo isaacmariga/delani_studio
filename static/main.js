cba6fa6af10c2247f44ea8578aee909ce90f7fea
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-18 18:39:38 +0300

    Adding the toggle js to portfolio

--- static/main.js
+++ static/main.js
@@ -1,1 +1,19 @@
-$('')
+$("#design_items").click(function() {
+    $("#design_text").toggle();
+    $("#design_img").toggle();
+});
+
+$("#development_items").click(function() {
+    $("#development_text").toggle();
+    $("#development_img").toggle();
+});
+
+$("#product_items").click(function() {
+    $("#product_text").toggle();
+    $("#product_img").toggle();
+});
+
+$("#product_items").click(function() {
+    $("#product_text").toggle();
+    $("#product_img").toggle();
+});
